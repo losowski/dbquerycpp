f7c4f3b8bc3d73e4d22e2c3132ed6a36f0cdb11b
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2019-02-05 22:45:53 +0000

    Added a main file

---
+++ democpp/main/main.cpp
@@ -0,0 +1,13 @@
+#include "neuron_schema.hpp"
+#include "tbody.hpp"
+
+#include <iostream>
+
+using namespace std;
+using namespace neuronSchema;
+
+int main (int argc, char * argv[])
+{
+	std::cout << "Neuron SQL schema tester" << std::endl;
+}
+
