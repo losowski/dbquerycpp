1cc901aa7392208d44a3786b6a6a2cdc18551143
Eugene Losowski-Gallagher <eugene.losowskigallagher@googlemail.com>
2019-02-13 22:58:00 +0000

    Added code for handling parent keys

--- democpp/neuron_schema.cpp
+++ democpp/neuron_schema.cpp
@@ -12,6 +12,7 @@ NeuronSchema::~NeuronSchema(void)
 {
 }
 
+//tBody
 shared_ptr<tBody> NeuronSchema::gtBody(int primaryKey)
 {
 	shared_ptr<tBody> obj(new tBody(this->m_dbconnection, primaryKey) );
@@ -19,11 +20,18 @@ shared_ptr<tBody> NeuronSchema::gtBody(int primaryKey)
 	return obj;
 }
 
+//tIndividual
 shared_ptr<tIndividual> NeuronSchema::gtIndividual(int primaryKey)
 {
 	shared_ptr<tIndividual> obj(new tIndividual(this->m_dbconnection, primaryKey) );
 	obj->selectRow();
 	return obj;
+}
+
+shared_ptr <tIndividuals_t> gtIndividualbytBody(int bodyId)
+{
+	//TODO: Figure out how to generate this list sensibly
+	// Needs some SQL here which breaks the design
 }
 
 }
--- democpp/neuron_schema.hpp
+++ democpp/neuron_schema.hpp
@@ -17,6 +17,8 @@ using namespace dbquery;
 
 namespace neuronSchema {
 
+typedef set < shared_ptr<tIndividual> > tIndividuals_t;
+
 class NeuronSchema : public dbquery::DBConnection
 {
 	public:
@@ -24,9 +26,14 @@ class NeuronSchema : public dbquery::DBConnection
 		~NeuronSchema(void);
 	public:
 		//void connect(void);
+		//tBody
 		shared_ptr<tBody> gtBody(int primaryKey);
+		//tIndividual
 		shared_ptr<tIndividual> gtIndividual(int primaryKey);
-		
+		//TODO: Add means to get all tIndividuals for a particular tBody key
+		shared_ptr <tIndividuals_t> gtIndividualbytBody(int bodyId);
+	protected:
+		shared_ptr <tIndividuals_t> tIndividualbytBody;
 };
 }
 #endif //NEURON_SCHEMA_HPP
